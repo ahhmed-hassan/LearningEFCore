d8c342d43a72e2b0a631a4f2a7adb0685bd66975
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-12-06 22:28:50 +0100

    Adding another connection string

---
+++ EFTest/Data/FakeTwitterV1/FakeTwitterV1DBContext.cs
@@ -0,0 +1,23 @@
+﻿using EFTest.Domain.TwitterV2;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFTest.Data.FakeTwitterV1;
+
+internal class FakeTwitterV1DBContext : DbContext
+{
+    private static IConfigurationRoot configuration => new ConfigurationBuilder().
+                AddJsonFile("appsettings.json").
+                Build();
+
+    public DbSet<User> users { get; set; }
+    public DbSet<Tweet> tweets { get; set; }
+    //public DbSet<Comment> comments { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("FakeTwitterV1")).
+            LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            ;
+    }
+}

4c22d3e1f045ffe6001e3e704833472e14534dca
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-12-07 10:42:29 +0100

    Setting up packages

---
+++ 10-Code-first/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace _10_Code_first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+
+               .Build();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

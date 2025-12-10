091074a1879b80b3e16e82ff885eef3297130f06
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-12-10 01:14:59 +0100

    Range Bound variants with boolean marker

--- RangeValueObject/RangeValueObject.Test/UnitTest1.cs
+++ RangeValueObject/RangeValueObject.Test/UnitTest1.cs
@@ -1,10 +1,67 @@
 ﻿namespace RangeValueObject.Test;
+using Xunit;
 
-public class UnitTest1
+// <summary>
+// Unit tests for RangeWithBound. 
+// Also tests the Range and RangeBound classes. The Range Bound classes should be tested 
+// indirectly through the Range class.
+/// </summary>
+public class RangeWithBoundTests
 {
-    [Fact]
-    public void Test1()
-    {
 
+    [Fact]
+    public void Contains_ValueWithinRange_ReturnsTrue()
+    {
+        var range = Range<int>.Create(5, 10);
+        Assert.True(range.Contains(7));
     }
+    [Fact]
+    public void Contains_ValueOutsideRange_ReturnsFalse()
+    {
+        var range = Range<int>.Create(5, 10);
+        Assert.False(range.Contains(4));
+        Assert.False(range.Contains(11));
+    }
+
+    [Fact]
+    public void Overlaps_OverlappingRanges_ReturnsTrue()
+    {
+        var range1 = Range<int>.Create(5, 10);
+        var range2 = Range<int>.Create(8, 12);
+        Assert.True(range1.Overlaps(range2));
+    }
+
+    [Fact]
+    public void Overlaps_NonOverlappingRanges_ReturnsFalse()
+    {
+        var range1 = Range<int>.Create(5, 10);
+        var range2 = Range<int>.Create(11, 15);
+        Assert.False(range1.Overlaps(range2));
+    }
+
+    [Fact]
+    public void Overlaps_RangesTouchingAtBoundary_ReturnsTrue()
+    {
+        var range1 = Range<int>.Create(5, 10);
+        var range2 = Range<int>.Create(10, 15);
+        Assert.True(range1.Overlaps(range2));
+    }
+
+    [Fact]
+    public void OpenEndedRange_ContainsValueGreaterThanStart_ReturnsTrue()
+    {
+        var range = Range<int>.OpenEndedRange(5);
+        Assert.True(range.Contains(10));
+    }
+    
+    [Fact]
+    public void OpenEndedRange_ContainsValueLessThanStart_ReturnsFalse()
+    {
+        var range = Range<int>.OpenEndedRange(5);
+        Assert.False(range.Contains(4));
+    }
+
+    
+    
+
 }
---
+++ RangeValueObject/RangeValueObject/RangeWithBound.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeValueObject;
+
+public readonly record struct RangeBound<T> : IComparable<RangeBound<T>>
+    where T : IComparable<T>
+{
+    private readonly T? _value;
+    private readonly bool _isInfinity;
+
+    private RangeBound(T? value, bool isInfinity) 
+    {
+        _value = value;
+        _isInfinity = isInfinity;
+    }
+    public int CompareTo(RangeBound<T> other)
+    {
+        if (_isInfinity && other._isInfinity) return 0;
+        if (_isInfinity) return 1;
+        if (other._isInfinity) return -1;
+        return _value!.CompareTo(other._value);
+    }
+    public int CompareTo(T other) => CompareTo(ValueOf(other));
+    public static RangeBound<T> Infinity() => new RangeBound<T>(default, true);
+    public static RangeBound<T> ValueOf(T value) => new RangeBound<T>(value, false);
+}
+
+public readonly record struct Range<T>
+    where T : IComparable<T>
+{
+    public RangeBound<T> Start { get; }
+    public RangeBound<T> End { get; }
+    public Range(RangeBound<T> start, RangeBound<T> end)
+    {
+        if (start.CompareTo(end) > 0)
+            throw new ArgumentException("Start must be less than or equal to End");
+        Start = start;
+        End = end;
+    }
+
+    public bool Contains(T value) =>
+        Start.CompareTo(value) <= 0 && End.CompareTo(value) >= 0;
+    
+    public static Range<T> Create(T start, T end) =>
+        new Range<T>(RangeBound<T>.ValueOf(start), RangeBound<T>.ValueOf(end));
+    public static Range<T> OpenEndedRange(T start) =>
+        new Range<T>(RangeBound<T>.ValueOf(start), RangeBound<T>.Infinity());
+
+    public bool Overlaps(Range<T> other) =>
+            Start.CompareTo(other.End) <= 0 && End.CompareTo(other.Start) >= 0;
+
+}

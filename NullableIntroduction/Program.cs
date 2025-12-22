083a8ada00c9367f3708bb73554790888a25b21c
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-12-22 18:53:55 +0100

    Survey Question and Response

---
+++ NullableIntroduction/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NullableIntroduction;
+
+var surveyRun = new SurveyRun();
+surveyRun.AddQuestion(QuestionType.YesNo, "Has your code ever thrown a NullReferenceException?");
+surveyRun.AddQuestion(new SurveyQuestion(QuestionType.Number, "How many times (to the nearest 100) has that happened?"));
+surveyRun.AddQuestion(QuestionType.Text, "What is your favorite color?");
+surveyRun.PerformSurvey(50);
---
+++ NullableIntroduction/SurveyQuestion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableIntroduction;
+
+internal enum QuestionType
+{
+    YesNo,
+    Text,
+    Number
+}
+internal class SurveyQuestion
+{
+    public string QuestionText { get; }
+    public QuestionType TypeOfQuestion { get; }
+
+    public SurveyQuestion(QuestionType typeOfQuestion, string text) =>
+       (TypeOfQuestion, QuestionText) = (typeOfQuestion, text);
+}
+
+internal class SurveyRun
+{
+    private List<SurveyQuestion> _surveyQuestions = new();
+
+    private List<SurveyResponse>? respondents;
+
+    public void AddQuestion(QuestionType questionType, string question) =>
+        AddQuestion(new SurveyQuestion(questionType, question));
+
+    public void AddQuestion(SurveyQuestion surveyQuestion) =>
+     _surveyQuestions.Add(surveyQuestion);
+
+    public void PerformSurvey(int numberOfRespondents)
+    {
+        respondents =  Enumerable
+            .Repeat(0, int.MaxValue)
+            .Select(_ => SurveyResponse.GetRandomId())
+            .Where(r => r.AnswerSurvey(_surveyQuestions))
+            .Take(numberOfRespondents)
+            .ToList();
+    }
+}

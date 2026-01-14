d8a8a533724f5e1354c8acbeaea2186414b5edce
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2026-01-14 09:53:41 +0100

    Illustrating the idea of a middleware

---
+++ Middleware-Implementation/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public interface IMiddleware{
+    public bool next(HttpContent httpContent);
+}
+
+public class FirstMiddleware : IMiddleware{
+    public bool next(HttpContent httpContent) => /*Asszme some work*/true;
+}
+
+public class SecondMiddleware : IMiddleware { 
+    public bool next(HttpContent httpContent) => /*Assume some different work*/false;
+}
+
+public class MiddlewareContainer{
+    private List<IMiddleware> _middlewares = new List<IMiddleware>();
+    public void AddMiddleware(IMiddleware middleware) => _middlewares.Add(middleware);
+    public bool process(HttpContent httpContent){
+        foreach (var middleware in _middlewares){
+            if(!middleware.next(httpContent))
+                return false;
+        }
+        return true;
+    }
+}
